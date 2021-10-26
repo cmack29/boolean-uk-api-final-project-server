c6590537f47e285ad48a64d3ed99fc5aaca2d710
unaben <unaben@yahoo.com>
2021-10-26 15:30:53 +0100

    createdOneRecipe on controller

--- src/resources/recipes/controller.js
+++ src/resources/recipes/controller.js
@@ -13,4 +13,35 @@ const getAll = async (req, res) => {
   }
 };
 
-module.exports = { getAll };
+const createOneRecipe = async (req, res) => {
+  console.log({ body: req.body.ingredients.name });
+  try {
+    const newRecipe = await prisma.recipe.create({
+      data: {
+        title: req.body.title,
+        description: req.body.description,
+        prepTime: req.body.prepTime,
+        cookingTime: req.body.cookingTime,
+        user: {
+          connect: { id: req.body.userId },
+        },
+        ingredients: {
+          create: [
+            {
+              name: req.body.ingredients.name,
+            },
+          ],
+        },
+      },
+      include: {
+        ingredients: true,
+      },
+    });
+    res.json({ data: newRecipe });
+  } catch (error) {
+    console.error("[ERROR] createOneRecipe: ", { error });
+    res.json({ error });
+  }
+};
+
+module.exports = { getAll, createOneRecipe };
--- src/resources/user/controller.js
+++ src/resources/user/controller.js
@@ -1,31 +1,29 @@
-const { prisma } = require("../utils/database")
+const prisma = require("../../utils/database");
 
 const createUserProfile = async (req, res) => {
-    try{
-        const newUser = await prisma.user.create({
-            data: {
-                userName: req.body.userName,
-                email: req.body.email
-            },
-            profile: {
-                create: [
-                    {
-                        ...req.body.profile
-                    }
-                ]
-            },
-            include: {
-                profile: true
-            }
-        })
-        res.json({ data: newUser })
-        console.log(newUser)
-    }
-    catch(error) {
-        console.error(error)
-        res.status(500).json({ error })
-    }
-}
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        userName: req.body.userName,
+        email: req.body.email,
+      },
+      profile: {
+        create: [
+          {
+            ...req.body.profile,
+          },
+        ],
+      },
+      include: {
+        profile: true,
+      },
+    });
+    res.json({ data: newUser });
+    console.log(newUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
+};
 
-
-module.exports = {createUserProfile}
+module.exports = { createUserProfile };

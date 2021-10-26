a279d61e9748c6622e0bd5f18fd48609b3698571
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-10-26 13:45:51 +0100

    Build `deleteUserProfile` controller

--- src/resources/user/controller.js
+++ src/resources/user/controller.js
@@ -1,31 +1,49 @@
-const { prisma } = require("../utils/database")
+const prisma = require("../../utils/database");
 
 const createUserProfile = async (req, res) => {
-    try{
-        const newUser = await prisma.user.create({
-            data: {
-                userName: req.body.userName,
-                email: req.body.email
-            },
-            profile: {
-                create: [
-                    {
-                        ...req.body.profile
-                    }
-                ]
-            },
-            include: {
-                profile: true
-            }
-        })
-        res.json({ data: newUser })
-        console.log(newUser)
-    }
-    catch(error) {
-        console.error(error)
-        res.status(500).json({ error })
-    }
-}
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        userName: req.body.userName,
+        email: req.body.email,
+      },
+      profile: {
+        create: [
+          {
+            ...req.body.profile,
+          },
+        ],
+      },
+      include: {
+        profile: true,
+      },
+    });
+    res.json({ data: newUser });
+    console.log(newUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
+};
 
+const deleteUserProfile = async (req, res) => {
+  const targetId = parseInt(req.params.id);
 
-module.exports = {createUserProfile}
+  try {
+    const deletedProfile = await prisma.profile.delete({
+      where: {
+        userId: targetId,
+      },
+    });
+
+    res.json({
+      message: `Profile of user with id:${targetId} has been deleted successfully!`,
+    });
+  } catch (error) {
+    console.error({ error: error.message });
+
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { createUserProfile, deleteUserProfile };
